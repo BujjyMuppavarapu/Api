a7f0fd7b216cce65f74c60b26704ef919f8e0046
BujjyMuppavarapu <bujjychowdary@gmail.com>
2025-09-18 11:59:08 +0530

    second commit

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+# Define an enum
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
-@app.get("/items/{item_id}")
-
-def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning pioneer!"}
+    if model_name == ModelName.resnet:
+        return {"model_name": model_name, "message": "Residual connections FTW!"}
+    return {"model_name": model_name, "message": "LeNet – old but gold"}
